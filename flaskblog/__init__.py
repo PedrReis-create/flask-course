a8f8fe904265ffc9848bcb19721f331757776abe
Pedro <ricardoepedroclash@gmail.com>
2026-07-15 15:42:50 -0300

    feat: configure SQLAlchemy database

---
+++ flaskblog/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e495bf39bfa108fe638c08a93c952141'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from flaskblog import routes
